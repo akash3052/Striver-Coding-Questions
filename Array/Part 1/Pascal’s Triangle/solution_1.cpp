c27fd4c54943fb678749eaa0715d0f55fb51a0ba
AKASH KUMAR <akash30521@gmail.com>
2022-05-14 15:46:18 +0530

    Create solution_1.cpp

---
+++ Array/Part 1/Pascal’s Triangle/solution_1.cpp
@@ -0,0 +1,30 @@
+//we need to insert 1 at the begining and also at the end.
+//And for the remaining elements (except 1st and last element) in a row , we are adding adjacent elements of previous row.
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> res(n);
+        if(n==0)
+            return res;
+        res[0].push_back(1);
+        if(n==1)
+            return res;
+        res[1].push_back(1);
+        res[1].push_back(1);
+        if(n==2)
+            return res;
+        for(int i=2;i<n;i++)
+        {
+            // vector<int> row;
+            res[i].push_back(1);
+            for(int j=0;j<res[i-1].size()-1;j++)
+            {
+                //Adding adjacent elements of previous row.
+                res[i].push_back(res[i-1][j]+res[i-1][j+1]);
+            }
+            res[i].push_back(1);
+            // res.push_back(row);
+        }
+        return res;
+    }
+};
